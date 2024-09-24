2c51da934f317bc6ee82b7eb8f634266708800ba
shen-liang <490712370@qq.com>
2024-09-24 20:21:18 +0800

    Initial Commit

---
+++ digg/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  digg
+//
+//  Created by hey on 2024/9/24.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ diggTests/diggTests.m
@@ -0,0 +1,36 @@
+//
+//  diggTests.m
+//  diggTests
+//
+//  Created by hey on 2024/9/24.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface diggTests : XCTestCase
+
+@end
+
+@implementation diggTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
